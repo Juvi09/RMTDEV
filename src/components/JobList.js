759ed6282c958f68e8e09cd6d3f22beea0cbd227
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2023-05-23 17:51:43 +0200

    refactored JobList component.

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -4,6 +4,31 @@ import {
     jobDetailsContentEl,
 } from '../common.js';
 
+const renderJobList = jobItems => {
+    jobItems.slice(0, 7).forEach(jobItem =>{
+        const newJobItemHTML = `<li class="job-item">
+        <a class="job-item__link" href="${jobItem.id}">
+            <div class="job-item__badge">${jobItem.badgeLetters}</div>
+            <div class="job-item__middle">
+                <h3 class="third-heading">${jobItem.title}</h3>
+                <p class="job-item__company">${jobItem.company}</p>
+                <div class="job-item__extras">
+                    <p class="job-item__extra"><i class="fa-solid fa-clock job-item__extra-icon"></i> ${jobItem.duration}</p>
+                    <p class="job-item__extra"><i class="fa-solid fa-money-bill job-item__extra-icon"></i>${jobItem.salary}</p>
+                    <p class="job-item__extra"><i class="fa-solid fa-location-dot job-item__extra-icon"></i>${jobItem.location}</p>
+                </div>
+            </div>
+            <div class="job-item__right">
+                <i class="fa-solid fa-bookmark job-item__bookmark-icon"></i>
+                <time class="job-item__time">${jobItem.daysAgo}d</time>
+            </div>
+        </a>
+    </li>
+    `;
+    jobListSearchEl.insertAdjacentHTML('beforeend', newJobItemHTML);
+  })
+};
+
 import renderSpinner from './Spinner.js';
  
 const clickHandler = () => {
@@ -109,3 +134,5 @@ const clickHandler = () => {
 };
 
 jobListSearchEl.addEventListener('submit', clickHandler);
+
+export default renderJobList;
