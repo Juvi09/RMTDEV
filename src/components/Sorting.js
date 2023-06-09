c0cb5b3c5e6eb25cde5daa2189b5bf48153680c5
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2023-06-09 21:52:12 +0200

    recent and relavant functionality.

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -5,7 +5,18 @@ import {
 } from '../common.js';
 
 const clickHandler = event => {
+    
+    // !get clicked button element
+    const clickedButtonEl = event.target.closest('.sorting__button');
 
+    // !stop function if no clicked button element.
+    if (!clickedButtonEl) return;
+
+    // ! check if intention is recent or relevant sorting
+    const recent = clickedButtonEl.className.includes('--recent') ? true : false;
+
+    // ! sort job items
+    
 };
 
 sortingEl.addEventListener('click', clickHandler);
