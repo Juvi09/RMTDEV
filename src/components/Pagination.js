a0e87195fb2ff4bb83d74be210c6b971e92bac29
Ntuthuko Ndlovu <ntuthuko.s.ndlovu@gmail.com>
2023-06-10 02:40:11 +0200

    pagination buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -8,18 +8,34 @@ import {
 } from '../common.js';
 import renderJobList from './JobList.js';
 
+const renderPaginationButtons = () => {
+    // display back button if we are on page 2 or further
+    if (state.currentPage >= 1) {
+        paginationBtnBackEl.classList.remove('pagination__button--hidden');
+    } else {
+        paginationBtnBackEl.classList.add('pagination__button--hidden');
+    }
+
+    // ! update page numbers
+    paginationBtnNextEl.textContent = state.currentPage + 1;
+    paginationBtnBackEl.textContent = state.currentPage - 1;
+};
+
 const clickHandler = event => {
-    // !get clicked button element
+    // ! get clicked button element
     const clickedButtonEl = event.target.closest('.pagination__button');
 
    // ! stop function if null
    if (!clickedButtonEl) return;
 
-   // !check if intention is next or back
+   // ! check if intention is next or back
    const nextPage = clickedButtonEl.className.includes('--next') ? true : false;
 
    // ! update state
    nextPage ? state.currentPage++ : state.currentPage--;
+
+   // ! render pagination buttons
+   renderPaginationButtons(); 
 
    // ! render job items for that page
    renderJobList();
